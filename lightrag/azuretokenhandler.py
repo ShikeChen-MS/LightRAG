1534b775c9ecbf95e316289af8b53048ff601537
Shike Chen <shikechen@microsoft.com>
2025-02-21 20:17:28 -0800

    work saving

--- lightrag/azuretokenhandler.py
+++ lightrag/azuretokenhandler.py
@@ -1,15 +1,26 @@
 import msal
 import jwt
+import enum
 from datetime import datetime, timedelta, timezone
 
+class TokenScope(enum.Enum):
+    CognitiveServices = "https://cognitiveservices.azure.com/.default offline_access"
+    CosmosDB_Postgres = "https://token.postgres.cosmos.azure.com/.default offline_access"
+    Storage = "https://storage.azure.com/.default offline_access"
 
 class AzureToken:
-    def __init__(self, ai_token: str, cosmos_token: str, refresh_token: str):
-        if not all([ai_token, cosmos_token, refresh_token]):
+    def __init__(self, token: str, refresh_token: str, token_scope: TokenScope):
+        if not all([token, refresh_token]):
             raise ValueError("All tokens must be provided")
-        self.ai_token = ai_token
-        self.cosmos_token = cosmos_token
+        self.token = token
+        # refresh token is long-lived and will be refreshed every time
+        # we get a new access token using it. So need to carry it
+        # along with access token.
         self.refresh_token = refresh_token
+        self.token_scope = token_scope
+
+    def check_scope(self, token_scope: TokenScope):
+        return self.token_scope.name == token_scope.name
 
 
 class AzureTokenHandler:
@@ -17,8 +28,6 @@ class AzureTokenHandler:
     __client_secret = None
     __authority = None
     __app_scope = [f"{__client_id}/.default offline_access"]
-    __ai_scope = ["https://cognitiveservices.azure.com/.default offline_access"]
-    __cosmos_scope = ["https://token.postgres.cosmos.azure.com/.default offline_access"]
     __confidential_app = None
 
     @classmethod
@@ -32,8 +41,11 @@ class AzureTokenHandler:
         return cls.__confidential_app
 
     @classmethod
-    def get_azure_token_by_user_token(cls, access_token: str):
+    def acquire_token_by_user_token(cls, access_token: str, token_scope: TokenScope):
         app = cls.get_confidential_app()
+        # Acquire token on behalf of the user with scope to app itself
+        # This will allow us to acquire token to any scope that
+        # the app has access to with the refresh token we get
         result = app.acquire_token_on_behalf_of(
             user_assertion=access_token, scopes=cls.__app_scope
         )
@@ -41,16 +53,11 @@ class AzureTokenHandler:
             raise ValueError(f"Error acquiring token: {result['error_description']}")
         if "refresh_token" not in result:
             raise ValueError("Refresh token not found in the result")
-        ai_token = app.acquire_token_by_refresh_token(
-            result.get("refresh_token"), cls.__ai_scope
-        )
-        cosmos_token = app.acquire_token_by_refresh_token(
-            ai_token.get("refresh_token"), cls.__cosmos_scope
-        )
+        token = app.acquire_token_by_refresh_token(result.get("refresh_token"), [token_scope.value])
         return AzureToken(
-            ai_token.get("access_token"),
-            cosmos_token.get("access_token"),
-            cosmos_token.get("refresh_token"),
+            token.get("access_token"),
+            token.get("refresh_token"),
+            token_scope
         )
 
     @classmethod
@@ -61,50 +68,45 @@ class AzureTokenHandler:
         if not res:
             return azure_token
         app = cls.get_confidential_app()
-        if azure_token.refresh_token:
-            ai_token = app.acquire_token_by_refresh_token(
-                azure_token.refresh_token, cls.__ai_scope
-            )
-            cosmos_token = app.acquire_token_by_refresh_token(
-                ai_token.get("refresh_token"), cls.__cosmos_scope
-            )
-            return AzureToken(
-                ai_token.get("access_token"),
-                cosmos_token.get("access_token"),
-                cosmos_token.get("refresh_token"),
-            )
-        else:
+        if not azure_token.refresh_token:
             raise ValueError("Refresh token is missing")
+        try:
+            token = app.acquire_token_by_refresh_token(
+                azure_token.refresh_token, [azure_token.token_scope.value]
+            )
+            azure_token.token = token.get("access_token")
+            azure_token.refresh_token = token.get("refresh_token")
+            return azure_token
+        except Exception as e:
+            raise ValueError(f"Error acquiring token: {str(e)}")
+
+    @classmethod
+    def acquire_token_by_refresh_token(cls, refresh_token: str, token_scope: TokenScope):
+        app = cls.get_confidential_app()
+        token = app.acquire_token_by_refresh_token(refresh_token, [token_scope.value])
+        return AzureToken(
+            token.get("access_token"),
+            token.get("refresh_token"),
+            token_scope
+        )
+
 
     @classmethod
     def __is_token_near_expire(cls, azure_token: AzureToken, threshold_seconds: int):
         # In theory since both token issued at same time, the remaining life should
         # be same, but for safety we check both and anyone of them near expire
         # we shall refresh both of them.
-        ai_decoded_token = jwt.decode(
-            azure_token.ai_token, options={"verify_signature": False}
-        )
-        cosmos_decoded_token = jwt.decode(
-            azure_token.cosmos_token, options={"verify_signature": False}
+        decoded_token = jwt.decode(
+            azure_token.token, options={"verify_signature": False}
         )
         # Extract the expiration time
-        ai_exp_timestamp = ai_decoded_token.get("exp")
-        cosmos_exp_timestamp = cosmos_decoded_token.get("exp")
-        if not ai_exp_timestamp:
-            raise ValueError("The AI access token does not contain an expiration time.")
-        if not cosmos_exp_timestamp:
-            raise ValueError(
-                "The Cosmos access token does not contain an expiration time."
-            )
+        exp_timestamp = decoded_token.get("exp")
+        if not exp_timestamp:
+            raise ValueError("The access token does not contain an expiration time.")
         # Convert the expiration time to a datetime object
-        ai_exp_time = datetime.fromtimestamp(ai_exp_timestamp, tz=timezone.utc)
-        cosmos_exp_time = datetime.fromtimestamp(cosmos_exp_timestamp, tz=timezone.utc)
+        exp_time = datetime.fromtimestamp(exp_timestamp, tz=timezone.utc)
         # Calculate the time remaining until expiration
         current_time = datetime.now(tz=timezone.utc)
-        ai_time_remaining = ai_exp_time - current_time
-        cosmos_time_remaining = cosmos_exp_time - current_time
+        time_remaining = exp_time - current_time
         # Check if the token is near expiration
-        is_near_expiry = ai_time_remaining < timedelta(seconds=threshold_seconds) or (
-            cosmos_time_remaining < timedelta(seconds=threshold_seconds)
-        )
-        return is_near_expiry
+        return time_remaining < timedelta(seconds=threshold_seconds)
